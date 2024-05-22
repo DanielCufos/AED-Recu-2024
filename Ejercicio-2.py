c7aadbbd859ecb949e820eb7de359301050681f5
DanielCufos <danielcufos111@gmail.com>
2024-05-22 18:43:01 -0300

    Guardado

---
+++ Ejercicio-2.py
@@ -0,0 +1,33 @@
+
+def numeroPositivo():
+    
+numero = []
+
+numeros = numero[]
+
+
+for num in range(7):
+    num = int(input("Decime un numero: "))
+    numero.append(num)
+
+
+
+
+print(numeros)
+
+NumPositivo = False
+
+for n in numero:   
+    if n > 0:
+        NumPositivo = True
+
+
+if NumPositivo:
+    print("hay positivos")
+
+else:
+    print("No hay positivos")
+
+
+
+
